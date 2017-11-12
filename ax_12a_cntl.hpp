71fa23804f09802925b551f922f33f9ef72532a3
ymp-kakeru <q104313k@mail.kyutech.jp>
2017-11-12 11:43:22 +0900

    hpp

---
+++ ax_12a_cntl.hpp
@@ -0,0 +1,298 @@
+#ifndef DYNAMIXEL_SDK_INCLUDE_DYNAMIXEL_SDK_AX12a_H_
+#define DYNAMIXEL_SDK_INCLUDE_DYNAMIXEL_SDK_AX12a_H_
+
+#if defined(__linux__) || defined(__APPLE__)
+#include <fcntl.h>
+#include <termios.h>
+#define STDIN_FILENO 0
+#elif defined(_WIN32) || defined(_WIN64)
+#include <conio.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <dynamixel_sdk/dynamixel_sdk.h>                                 // Uses Dynamixel SDK library
+
+#include <urg_sensor.h>
+#include <urg_utils.h>
+
+// Control table address
+#define ADDR_MX_TORQUE_ENABLE           24                  // Control table address is different in Dynamixel model
+#define ADDR_MX_GOAL_POSITION           30
+#define ADDR_MX_PRESENT_POSITION        36
+#define ADDR_MOVING_SPEED               32
+#define ADDR_TORQUE_LIMIT				34
+
+// Protocol version
+#define PROTOCOL_VERSION                1.0                 // See which protocol version is used in the Dynamixel
+
+// Default setting
+#define DXL_ID                          1                   // Dynamixel ID: 1
+#define BAUDRATE                        1000000
+#define DEVICENAME                      "/dev/ttyUSB0"      // Check which port is being used on your controller
+                                                            // ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+#define TORQUE_ENABLE                   1                   // Value for enabling the torque
+#define TORQUE_DISABLE                  0                   // Value for disabling the torque
+#define DXL_MINIMUM_POSITION_VALUE      500                 // Dynamixel will rotate between this value
+#define DXL_MAXIMUM_POSITION_VALUE      672                // and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+#define DXL_MOVING_STATUS_THRESHOLD     10                  // Dynamixel moving status threshold
+#define DXL_MOVING_SPEED                40                  // Moving speed = 1/4 rpm
+#define DXL_TORQUE_LIMIT				950
+
+#define ESC_ASCII_VALUE                 0x1b
+
+class DXL_AX12a
+{
+	public:
+		DXL_AX12a() ;
+		~DXL_AX12a();
+
+	private:
+		DXL_cntl() ;
+		int getch() ;
+		int kbhit(void) ;
+
+		// URG /////////////////////////////////////////
+  		void OpenURG();
+  		void CloseURG();
+  		bool GetURGData(ros::Time& t);
+
+		string ip_address;
+		int ip_port;
+		string serial_port;
+		int serial_baud;
+
+		urg_t urg;
+/////////////////////////////////////////////////
+
+} ;
+
+
+
+
+DXL_AX12a::DXL_AX12a()
+{
+//OpenURG();
+
+}
+
+DXL_AX12a::~DXL_AX12a()
+{
+		// Disable Dynamixel Torque
+  	dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  	if (dxl_comm_result != COMM_SUCCESS)
+  	{
+   		printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  	}
+  	else if (dxl_error != 0)
+  	{
+    	printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  	}
+
+  	// Close port
+  	portHandler->closePort();
+  	}
+
+  	CloseURG();
+}
+
+
+int getch()
+{
+	#if defined(__linux__) || defined(__APPLE__)
+	struct termios oldt, newt;
+	int ch;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+ 	newt.c_lflag &= ~(ICANON | ECHO);
+ 	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  	ch = getchar();
+  	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  	return ch;
+	#elif defined(_WIN32) || defined(_WIN64)
+  	return _getch();
+	#endif
+}
+
+int kbhit()
+{
+	#if defined(__linux__) || defined(__APPLE__)
+  	struct termios oldt, newt;
+  	int ch;
+  	int oldf;
+
+  	tcgetattr(STDIN_FILENO, &oldt);
+  	newt = oldt;
+  	newt.c_lflag &= ~(ICANON | ECHO);
+	  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+ 	 oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	  ch = getchar();
+
+  	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  	if (ch != EOF)
+  	{
+   	 ungetc(ch, stdin);
+   	 return 1;
+  	}
+
+  	return 0;
+	#elif defined(_WIN32) || defined(_WIN64)
+  	return _kbhit();
+	#endif	
+}
+
+DXL_cntl()
+{
+	// Initialize PortHandler instance
+  // Set the port path
+  // Get methods and members of PortHandlerLinux or PortHandlerWindows
+  dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+
+  // Initialize PacketHandler instance
+  // Set the protocol version
+  // Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+  dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+  int index = 0;
+  int dxl_comm_result = COMM_TX_FAIL;             // Communication result
+  int dxl_goal_position[2] = {DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE};         // Goal position
+
+  uint8_t dxl_error = 0;                          // Dynamixel error
+  uint16_t dxl_present_position = 0;              // Present position
+
+  int end_count = 3 ;
+  int count = 0;
+
+  // Open port
+  if (portHandler->openPort())
+  {
+    printf("Succeeded to open the port!\n");
+  }
+  else
+  {
+    printf("Failed to open the port!\n");
+    printf("Press any key to terminate...\n");
+    getch();
+    return 0;
+  }
+
+  // Set port baudrate
+  if (portHandler->setBaudRate(BAUDRATE))
+  {
+    printf("Succeeded to change the baudrate!\n");
+  }
+  else
+  {
+    printf("Failed to change the baudrate!\n");
+    printf("Press any key to terminate...\n");
+    getch();
+    return 0;
+  }
+
+  // Enable Dynamixel Torque
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0)
+  {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+  else
+  {
+    printf("Dynamixel has been successfully connected \n");
+  }
+
+  // Moving Speed write control table
+  dxl_comm_result = packetHandler->write2ByteTxRx(portHandler, DXL_ID, ADDR_MOVING_SPEED, DXL_MOVING_SPEED, &dxl_error);
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+    else if (dxl_error != 0)
+    {
+      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    }
+
+  // Torque Limit set up
+    dxl_comm_result = packetHandler->write2ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_LIMIT, DXL_TORQUE_LIMIT, &dxl_error);
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+    else if (dxl_error != 0)
+    {
+      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    }
+
+  while(1)
+  {
+    printf("only 3 times : now = %d / 3\n\n\n\n",count);
+    if (count == end_count)
+      break;
+
+    // Write goal position
+    dxl_comm_result = packetHandler->write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, dxl_goal_position[index], &dxl_error);
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+    else if (dxl_error != 0)
+    {
+      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    }
+
+    do
+    {
+      // Read present position
+      dxl_comm_result = packetHandler->read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION, &dxl_present_position, &dxl_error);
+      if (dxl_comm_result != COMM_SUCCESS)
+      {
+        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+      }
+      else if (dxl_error != 0)
+      {
+        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+      }
+
+      printf("[ID:%03d] GoalPos:%03d  PresPos:%03d\n", DXL_ID, dxl_goal_position[index], dxl_present_position);
+      
+    }while((abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+    //count += 1 ;
+    // Change goal position
+    if (index == 0)
+    {
+      index = 1;
+    }
+    else
+    {
+      index = 0;
+    }
+  }
+}
+
+~DXL_cntl()
+{
+	// Disable Dynamixel Torque
+  	dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  	if (dxl_comm_result != COMM_SUCCESS)
+  	{
+   		printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  	}
+  	else if (dxl_error != 0)
+  	{
+    	printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  	}
+
+  	// Close port
+  	portHandler->closePort();
+  	}
+}
+
